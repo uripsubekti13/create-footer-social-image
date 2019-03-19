d4644522d10d58c49894670dc6151983cd7e87ca
Urip Subekti <urip.subekti13@gmail.com>
2019-03-19 18:02:35 +0700

    fix

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const { SOCIALS } = require('./icons')
 class CreateFooter {
     async getFooter(data) {
         try {
-            const base = 30
+            const base = 35
             const canvas = createCanvas(500, (base+3) * data.length);
             const ctx = canvas.getContext('2d');
             ctx.fillStyle = "white";
             ctx.fillRect(0, 0, 500, (base+3) * data.length);
             for (let index = 0; index < data.length; index++) {
                 const obj = data[index];
-                ctx.drawImage(await loadImage(SOCIALS[obj.key]), 0, 2 + (index * (base+1)), base, base)
+                ctx.drawImage(await loadImage(SOCIALS[obj.key]), 0, 4 + (index * (base+1)), base-3, base-3)
                 ctx.font = '28px Arial'
                 ctx.fillStyle = "black";
-                ctx.fillText(obj.value, (base+7), ((index + 1) * (base+1)) - 3)
+                ctx.fillText(obj.value, (base+9), ((index + 1) * (base+1)) - 5)
             }
             return canvas.toDataURL();
         } catch (e) {
@@ -23,7 +23,7 @@ class CreateFooter {
     }
 }
 
-const a = new CreateFooter()
-a.getFooter([{key: 'IG', value: 'Urip Subekti'}, {key: 'PHONE', value: '087722171686'}]).then(console.log)
+// const a = new CreateFooter()
+// a.getFooter([{key: 'FB', value: 'Urip Subekti'}, {key: 'PHONE', value: '087722171686'}]).then(console.log)
 
-// module.exports = new CreateFooter()
+module.exports = new CreateFooter()
