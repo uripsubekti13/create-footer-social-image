a43faf15e98673eb599129352c7dfa4acde3eb55
Urip Subekti <urip.subekti13@gmail.com>
2019-03-19 13:47:23 +0700

    add error

--- index.js
+++ index.js
@@ -3,68 +3,88 @@ const { SOCIALS } = require('./icons')
 
 class CreateFooter {
     async getFB(text = 'Facebook') {
-        const canvas = createCanvas(500, 23);
-        const ctx = canvas.getContext('2d');
-        const img = await loadImage(SOCIALS.FB);
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, 500, 23);
-        ctx.drawImage(img, 0, 0, 23, 23);
-        ctx.font = '22px Arial'
-        ctx.fillStyle = "black";
-        ctx.fillText(text, 30, 20)
-        return canvas.toDataURL()
+        try {
+            const canvas = createCanvas(500, 23);
+            const ctx = canvas.getContext('2d');
+            const img = await loadImage(SOCIALS.FB);
+            ctx.fillStyle = "white";
+            ctx.fillRect(0, 0, 500, 23);
+            ctx.drawImage(img, 0, 0, 23, 23);
+            ctx.font = '22px Arial'
+            ctx.fillStyle = "black";
+            ctx.fillText(text, 30, 20)
+            return canvas.toDataURL()
+        } catch (error) {
+            throw error;
+        }
     }
 
     async getEmail(text = 'Email') {
-        const canvas = createCanvas(500, 23);
-        const ctx = canvas.getContext('2d');
-        const img = await loadImage(SOCIALS.EMAIL);
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, 500, 23);
-        ctx.drawImage(img, 0, 0, 23, 23);
-        ctx.font = '22px Arial'
-        ctx.fillStyle = "black";
-        ctx.fillText(text, 30, 20)
-        return canvas.toDataURL()
+        try {
+            const canvas = createCanvas(500, 23);
+            const ctx = canvas.getContext('2d');
+            const img = await loadImage(SOCIALS.EMAIL);
+            ctx.fillStyle = "white";
+            ctx.fillRect(0, 0, 500, 23);
+            ctx.drawImage(img, 0, 0, 23, 23);
+            ctx.font = '22px Arial'
+            ctx.fillStyle = "black";
+            ctx.fillText(text, 30, 20)
+            return canvas.toDataURL()
+        } catch (error) {
+            throw error;
+        }
     }
 
     async getIG(text = 'Instagram') {
-        const canvas = createCanvas(500, 23);
-        const ctx = canvas.getContext('2d');
-        const img = await loadImage(SOCIALS.IG);
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, 500, 23);
-        ctx.drawImage(img, 0, 0, 23, 23);
-        ctx.font = '22px Arial'
-        ctx.fillStyle = "black";
-        ctx.fillText(text, 30, 20)
-        return canvas.toDataURL()
+        try {
+            const canvas = createCanvas(500, 23);
+            const ctx = canvas.getContext('2d');
+            const img = await loadImage(SOCIALS.IG);
+            ctx.fillStyle = "white";
+            ctx.fillRect(0, 0, 500, 23);
+            ctx.drawImage(img, 0, 0, 23, 23);
+            ctx.font = '22px Arial'
+            ctx.fillStyle = "black";
+            ctx.fillText(text, 30, 20)
+            return canvas.toDataURL()
+        } catch (error) {
+            throw error;
+        }
     }
 
     async getPhone(text = 'Phone') {
-        const canvas = createCanvas(500, 23);
-        const ctx = canvas.getContext('2d');
-        const img = await loadImage(SOCIALS.PHONE);
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, 500, 23);
-        ctx.drawImage(img, 0, 0, 23, 23);
-        ctx.font = '22px Arial'
-        ctx.fillStyle = "black";
-        ctx.fillText(text, 30, 20)
-        return canvas.toDataURL()
+        try {
+            const canvas = createCanvas(500, 23);
+            const ctx = canvas.getContext('2d');
+            const img = await loadImage(SOCIALS.PHONE);
+            ctx.fillStyle = "white";
+            ctx.fillRect(0, 0, 500, 23);
+            ctx.drawImage(img, 0, 0, 23, 23);
+            ctx.font = '22px Arial'
+            ctx.fillStyle = "black";
+            ctx.fillText(text, 30, 20)
+            return canvas.toDataURL()
+        } catch (error) {
+            throw error;
+        }
     }
 
     async getTwitter(text = 'Twitter') {
-        const canvas = createCanvas(500, 23);
-        const ctx = canvas.getContext('2d');
-        const img = await loadImage(SOCIALS.TWITTER);
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, 500, 23);
-        ctx.drawImage(img, 0, 0, 23, 23);
-        ctx.font = '22px Arial'
-        ctx.fillStyle = "black";
-        ctx.fillText(text, 30, 20)
-        return canvas.toDataURL()
+        try {
+            const canvas = createCanvas(500, 23);
+            const ctx = canvas.getContext('2d');
+            const img = await loadImage(SOCIALS.TWITTER);
+            ctx.fillStyle = "white";
+            ctx.fillRect(0, 0, 500, 23);
+            ctx.drawImage(img, 0, 0, 23, 23);
+            ctx.font = '22px Arial'
+            ctx.fillStyle = "black";
+            ctx.fillText(text, 30, 20)
+            return canvas.toDataURL()
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
